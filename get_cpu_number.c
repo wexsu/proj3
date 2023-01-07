a9d4a2391634a9050b69a7079d38ad97f034bb75
wexsu <85936457+wexsu@users.noreply.github.com>
2023-01-07 19:30:39 +0800

    Create get_cpu_number.c

---
+++ get_cpu_number.c
@@ -0,0 +1,13 @@
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/string.h>
+#include <linux/init_task.h>
+#include <linux/uaccess.h>
+#include <linux/syscalls.h>
+#include <linux/sched.h>
+
+SYSCALL_DEFINE0(get_cpu_number){
+    unsigned   recent_used_cpu;
+    recent_used_cpu=current->cpu;
+    return cpu;
+}
